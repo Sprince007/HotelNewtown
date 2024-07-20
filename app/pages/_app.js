2809bea83d96b81679c47ddf4750e45aefd8c406
Sprince007 <Olanrewaju.bidemi@ictuniversity.edu.cm>
2024-07-20 08:13:45 +0100

    insights

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import Modal from 'react-modal';
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     Modal.setAppElement('#__next'); // Set the app element once globally
   }, []);
-
+<SpeedInsights/>
   return <Component {...pageProps} />;
 }
 
